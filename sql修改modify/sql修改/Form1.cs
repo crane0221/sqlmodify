564e790e8a99b7acd191ba3a848dfc3d97405861
crane0221 <75875765+crane0221@users.noreply.github.com>
2020-12-29 16:35:09 +0800

    Add files via upload

---
+++ sql修改modify/sql修改/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace sql修改
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection con = cn1.cn)
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText =
+                    $"Update Account Set Email = 'asd1' Where Name = 'Gin'";
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
